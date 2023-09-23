0e56145c927c3a57e1ae9803760b5e1bfe8919f1
kaiden001 <116476264+kaiden001@users.noreply.github.com>
2023-09-23 13:56:34 +0800

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,15 +95,15 @@ modalCloses.forEach((modalClose) => {
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiper = new Swiper(".swiper-container", {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  mousewheel: true,
-  keyboard: true,
 });
 /*==================== TESTIMONIAL ====================*/
 
